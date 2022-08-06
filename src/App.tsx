d534dd87c4d2b45096872e56fd2387e67158dd70
Helge <elitaer@gmail.com>
2022-08-06 23:43:29 +0200

    make it work

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,114 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import "./settimer.css";
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(60 * 60 * 2 - 361);
-  const interval = useRef<any>();
+  const searchParam = window.location.search;
+  const searchParams = new URLSearchParams(searchParam);
+
+  return (
+    <div className="App">
+      {searchParam ? (
+        <CounterWrapper
+          time={searchParams.get("time") ?? "00:00:00"}
+          msg={searchParams.get("msg") ?? "Short break! Back in.."}
+        />
+      ) : (
+        <SetTimer />
+      )}
+    </div>
+  );
+}
+
+const SetTimer = () => {
+  function dateFromStr(date: string) {
+    let tempTime = date.split(":");
+    let dt = new Date();
+    dt.setHours(+tempTime[0]);
+    dt.setMinutes(+tempTime[1]);
+    dt.setSeconds(+tempTime[2]);
+    return dt;
+  }
+
+  const timerClickHandler = () => {
+    const setTime = dateFromStr(time);
+    const tmp = setTime.getTime() - new Date().getTime();
+    const timeDiff = new Date(tmp).toISOString().substring(11, 19);
+    return timeDiff;
+  };
+
+  const [time, setTime] = useState("00:00:00");
+  const [msg, setMessage] = useState("Short break! Back in..");
+
+  return (
+    <div className="wrapper-timer">
+      <input
+        type="time"
+        onChange={(e) => setTime(e.target.value)}
+        defaultValue={"00:00:00"}
+        step={1}
+        style={{ width: "300px", fontSize: "1.5rem", textAlign: "center" }}
+      />
+      <input
+        type="text"
+        onChange={(e) => setMessage(e.target.value)}
+        style={{ width: "300px", fontSize: "1.5rem", textAlign: "center" }}
+        placeholder="Short break! Back in.."
+        defaultValue={msg}
+      />
+      <div className="wrapper-buttons">
+        <button
+          onClick={() => {
+            const time = timerClickHandler();
+            location.href = `/?time=${time}&msg=${msg}`;
+          }}
+        >
+          Set Timer
+        </button>
+        <span>or</span>
+        <button
+          onClick={() => {
+            location.href = `/?time=${time}&msg=${msg}`;
+          }}
+        >
+          Set Countdown
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const CounterWrapper = ({ time, msg }: { time: string; msg: string }) => {
+  const totalSecs = time.split(":").reduce((acc, time) => 60 * acc + +time, 0);
+  const [count, setCount] = useState(totalSecs);
+  const interval = useRef<ReturnType<typeof setTimeout>>();
+
   useEffect(() => {
     interval.current = setInterval(() => {
       setCount((count) => count - 1);
     }, 1000);
     return () => clearInterval(interval.current);
   }, []);
-  if (count === 0) {
+
+  if (count < 0) {
     clearTimeout(interval.current);
+    setCount(0);
   }
+
   return (
-    <div className="App">
-      <h1>Starting in</h1>
+    <>
+      <h1>{msg}</h1>
       <div className="wrapper">
         <Counter value={count} variant="hours" />
         <Counter value={count} variant="minutes" />
         <Counter value={count} variant="seconds" />
       </div>
-    </div>
-  );
-}
-
-export default App;
-
-type Variant = "hours" | "minutes" | "seconds";
-enum VariantEnum {
-  hours = 24 * 60 * 60,
-  minutes = 60 * 60,
-  seconds = 60,
-}
-const Counter = ({ value, variant }: { value: number; variant: Variant }) => {
-  const circle = useRef<SVGCircleElement>(null);
-  let displayValue = value % 60;
-  if (variant === "hours") {
-    displayValue = Math.floor((24 * value) / VariantEnum.hours) % 24;
-  } else if (variant === "minutes") {
-    displayValue = Math.floor((60 * value) / VariantEnum.minutes) % 60;
-  }
-  // console.log(displayValue);
-  useEffect(() => {
-    if (!circle.current) return;
-    const length =
-      2 * Math.PI * parseInt(circle.current?.getAttribute("r") || "0");
-    circle.current.style.strokeDasharray = `${length} ${length}`;
-    const percentage = (value / VariantEnum[variant]) % 100;
-    const offset = (1 - (percentage % 1)) * length;
-    circle.current.style.strokeDashoffset = `${offset}`;
-    // circle.current.getBoundingClientRect();
-  }, [circle, value]);
-
-  return (
-    <div className="item">
-      <span>{displayValue}</span>
-      <>
-        <svg width="160" height="160">
-          <circle
-            r="60"
-            cx="80"
-            cy="80 "
-            strokeLinecap="round"
-            strokeWidth="4"
-            fill="none"
-            stroke="#141414"
-          ></circle>
-        </svg>
-        <svg width="160" height="160">
-          <defs>
-            <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-              <stop offset="0%" stopColor="#00bc9b" />
-              <stop offset="100%" stopColor="#5eaefd" />
-            </linearGradient>
-          </defs>
-          <g>
-            <circle
-              id="circle"
-              ref={circle}
-              r="60"
-              cy="80"
-              cx="80"
-              strokeWidth="4"
-              stroke="url(#gradient)"
-              fill="none"
-              style={{ transition: "stroke-dashoffset 1s ease-out" }}
-            />
-          </g>
-        </svg>
-      </>
-    </div>
+      <div className="wrapper">
+        <a href="/">reset</a>
+      </div>
+    </>
   );
 };
+
+export default App;
---
+++ src/Counter.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useRef } from "react";
+import "./counter.css";
+
+type Variant = "hours" | "minutes" | "seconds";
+enum VariantEnum {
+  hours = 24 * 60 * 60,
+  minutes = 60 * 60,
+  seconds = 60,
+}
+
+const Counter = ({ value, variant }: { value: number; variant: Variant }) => {
+  const circle = useRef<SVGCircleElement>(null);
+  let displayValue = value % 60;
+
+  if (variant === "hours") {
+    displayValue = Math.floor((24 * value) / VariantEnum.hours) % 24;
+  } else if (variant === "minutes") {
+    displayValue = Math.floor((60 * value) / VariantEnum.minutes) % 60;
+  }
+
+  useEffect(() => {
+    if (!circle.current) return;
+    const length =
+      2 * Math.PI * parseInt(circle.current?.getAttribute("r") || "0");
+    circle.current.style.strokeDasharray = `${length} ${length}`;
+    const percentage = (value / VariantEnum[variant]) % 100;
+    const offset = (1 - (percentage % 1)) * length;
+    circle.current.style.strokeDashoffset = `${offset}`;
+  }, [circle, value]);
+
+  return (
+    <div className="item">
+      <span>{displayValue < 10 ? `0${displayValue}` : displayValue}</span>
+      <>
+        <svg width="160" height="160">
+          <circle
+            r="60"
+            cx="80"
+            cy="80 "
+            strokeLinecap="round"
+            strokeWidth="4"
+            fill="none"
+            stroke="#141414"
+          ></circle>
+        </svg>
+        <svg width="160" height="160">
+          <defs>
+            <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="0%" stopColor="#00bc9b" />
+              <stop offset="100%" stopColor="#5eaefd" />
+            </linearGradient>
+          </defs>
+          <g>
+            <circle
+              id="circle"
+              ref={circle}
+              r="60"
+              cy="80"
+              cx="80"
+              strokeWidth="4"
+              stroke="url(#gradient)"
+              fill="none"
+              style={{ transition: "stroke-dashoffset 1s ease-out" }}
+            />
+          </g>
+        </svg>
+      </>
+    </div>
+  );
+};
+
+export default Counter;
